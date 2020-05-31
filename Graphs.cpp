54c680acf3acf7c02893eaccf31919746f6ea253
Alaa Iraqy <48126729+AlaaIraqy@users.noreply.github.com>
2020-05-31 06:09:37 +0200

    Add files via upload

---
+++ Graphs.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <list>
+using namespace std;
+void addEdge(vector<int> adj[], int u, int v)
+{
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+int output(vector<int> adj[],int val, int k,int v) {
+    int out = 0;
+    list<int> Q;
+    bool visited[v];
+    for (int i = 0; i < v; i++) {
+        visited[i] = false;
+    }
+    for (int i = 0; i < adj[val].size(); i++) {
+        if (!visited[adj[val][i]]) {
+            Q.push_back(adj[val][i]);
+            visited[adj[val][i]] = true;
+        }
+    }
+    while (k > 1) {
+        int n = Q.size();
+        for (int i = 0; i < n; i++) {
+            int temp = Q.front();
+            for (int i = 0; i < adj[temp].size(); i++) {
+                if (!visited[adj[temp][i]]) {
+                    Q.push_back(adj[temp][i]);
+                    visited[adj[temp][i]] = true;
+                }
+            }
+            Q.pop_front();
+            Q.remove(val);
+        }
+        k--;
+    }
+    out = Q.size();
+    return out;
+
+}
+int main() {
+    int Nv, n, u, v,start,k;
+    cout << "Enter number of vertices" << endl;
+    cin >> Nv;
+    vector<int> adj[Nv];
+    cout << "Enter number of edges" << endl;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cout << "Enter u" << endl;
+        cin >> u;
+        cout << "Enter v" << endl;
+        cin >> v;
+        addEdge(adj, u, v);
+    }
+    cout << "Enter starting vertex" << endl;
+    cin >> start;
+    cout << "Enter K" << endl;
+    cin >> k;
+    int out = output(adj, start, k,Nv);
+    cout << "there are " << out << " people with " << k << " connections away starting from " << start << endl;
+
+    return 0;
+}
